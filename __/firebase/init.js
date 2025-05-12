1bf9e3fcf8f0c347248877cc0957a054089265dd
dfbaron <df.baron10@uniandes.edu.co>
2025-05-11 23:07:01 -0500

    Initial commit - restored project from Firebase

---
+++ __/firebase/init.js
@@ -0,0 +1,9 @@
+if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js');
+firebase.initializeApp({
+  "apiKey": "AIzaSyB4ZPcCFOEZJ1LOug-Acd2lU-YMriE-Zyo",
+  "authDomain": "dfbaron-webpage.firebaseapp.com",
+  "databaseURL": "",
+  "messagingSenderId": "413988449299",
+  "projectId": "dfbaron-webpage",
+  "storageBucket": "dfbaron-webpage.appspot.com"
+});
